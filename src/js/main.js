fc0ecf1b3df862653ac4e1390ea87496f0cd2f8f
julieandersson <juan2301@student.miun.se>
2024-01-26 11:39:15 +0100

    skapar grund för indexhtml samt javascript

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const url = "https://dahlgren.miun.se/ramschema_ht23.php";
+
+window.onload = init;
+
+async function init() {
+    try {
+        //
+    } catch {
+        document.getElementById("error").innerHTML = "<p>Något gick fel, prova igen senare!</p>";
+    }
+}
