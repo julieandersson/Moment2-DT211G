d3fec25bf2ad6674e38c372487112126aa9a3e90
julieandersson <juan2301@student.miun.se>
2024-01-26 12:12:30 +0100

    Börjar med att skriva ut kurskoder i tabellen till skärmen

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,35 @@
 "use strict";
 
+
 const url = "https://dahlgren.miun.se/ramschema_ht23.php";
 
 window.onload = init;
 
 async function init() {
     try {
-        //
+        // Fetch-anrop
+        const response = await fetch(url);
+        const courses = await response.json();
+
+        displayCourses(courses);
     } catch {
         document.getElementById("error").innerHTML = "<p>Något gick fel, prova igen senare!</p>";
     }
+}
+
+function displayCourses(courses) {
+    const codeEl = document.getElementById("code");
+
+    courses.forEach((course) => {
+        // Loopa igenom och skriva ut till DOM 
+        let newRow = document.createElement("tr");
+        
+        // Kurskod
+        let newCodeId = document.createElement("td");
+        let newCodeName = document.createTextNode(course.code);
+        newCodeId.appendChild(newCodeName);
+        newRow.appendChild(newCodeId);
+
+        codeEl.appendChild(newRow);
+    });
 }
